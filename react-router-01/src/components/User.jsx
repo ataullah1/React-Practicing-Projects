6f47f13cbd2589bac903ad75f64a3deee388787d
Md Ataullah <ataullahm100@gmail.com>
2024-03-20 16:00:37 +0600

    App jsx adding

--- react-router-01/src/components/User.jsx
+++ react-router-01/src/components/User.jsx
@@ -1,10 +1,14 @@
+import PropTypes from 'prop-types';
 const User = ({ dta }) => {
   console.log(dta);
   return (
     <div>
-      <h1></h1>
+      <h1>Hello</h1>
     </div>
   );
 };
 
+User.propTypes = {
+  dta: PropTypes.object,
+};
 export default User;
--- react-router-01/src/main.jsx
+++ react-router-01/src/main.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
-import Home from './components/Home';
+// import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact.jsx';
 import Users from './components/Users.jsx';
+import App from './App.jsx';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Home />,
+    element: <App />,
     children: [
       {
         path: '/about',
