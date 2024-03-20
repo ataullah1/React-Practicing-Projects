57fbc690286c26138148242b822115828191fd2e
Md Ataullah <ataullahm100@gmail.com>
2024-03-20 16:06:22 +0600

    Data adding api

--- react-router-01/src/components/User.jsx
+++ react-router-01/src/components/User.jsx
@@ -1,9 +1,20 @@
 import PropTypes from 'prop-types';
 const User = ({ dta }) => {
+  const { name, email, id, website } = dta;
   console.log(dta);
   return (
-    <div>
-      <h1>Hello</h1>
+    <div
+      style={{
+        border: '2px solid hotpink',
+        margin: '0 0 10px 0',
+        textAlign: 'center',
+      }}
+    >
+      <h1>
+        {id}- {name}
+      </h1>
+      <h2>{email}</h2>
+      <h2>{website}</h2>
     </div>
   );
 };
