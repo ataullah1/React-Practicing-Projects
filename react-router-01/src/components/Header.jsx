902d356a6df277dd8536df7be410086eaadfc577
Md Ataullah <ataullahm100@gmail.com>
2024-03-20 12:19:37 +0600

    React Router Nav Practicesing

---
+++ react-router-01/src/components/Header.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <div>
+      <ul>
+        <li>Heme</li>
+        <li>About</li>
+        <li>Blog</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Header;
--- react-router-01/src/main.jsx
+++ react-router-01/src/main.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import App from './App.jsx';
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom';
+import './index.css';
+import Header from './components/Header';
+import Home from './components/Home';
+import About from './components/About';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+    children: [
+      {
+        path: '/header',
+        element: <Header />,
+      },
+      {
+        path: '/about',
+        element: <About />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
