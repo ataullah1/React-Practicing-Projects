e74354b6027dd069ceaa1bd1995d49cf6e99c739
Md Ataullah <ataullahm100@gmail.com>
2024-03-21 15:25:44 +0600

    snowyblaze

---
+++ Snowyblaze/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Nav from '../components/Nav';
+import Bookmarks from './Bookmarks';
+
+const Home = () => {
+  return (
+    <div>
+      <Nav />
+      <Bookmarks />
+    </div>
+  );
+};
+
+export default Home;
