646a0de916d12d290d278ca8a476211ec96d21bb
Md Ataullah <ataullahm100@gmail.com>
2024-03-21 14:08:07 +0600

    afasdfas

--- React-Router_Project-2/src/Home.jsx
+++ React-Router_Project-2/src/Home.jsx
@@ -1,11 +1,14 @@
+import { Outlet } from 'react-router-dom';
 import Footer from './components/Footer';
 import Header from './components/Header';
 
 const Home = () => {
   return (
-    <div className="bg-red-100">
+    <div className="">
       <Header />
-      <div className="w-11/12 mx-auto min-h-screen"></div>
+      <div className="w-11/12 mx-auto min-h-screen">
+        <Outlet />
+      </div>
       <Footer />
     </div>
   );
---
+++ React-Router_Project-2/src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Start React Routing for your project</h1>
+    </div>
+  );
+};
+
+export default HomePage;
