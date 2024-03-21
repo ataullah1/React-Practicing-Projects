a2dade0b0ac8530a4bba595061caf343537036c1
Md Ataullah <ataullahm100@gmail.com>
2024-03-21 07:39:50 +0600

    LKoding add

--- react-router-01/src/components/Home.jsx
+++ react-router-01/src/components/Home.jsx
@@ -1,11 +1,16 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigation } from 'react-router-dom';
 import Header from './Header';
-
 const Home = () => {
+  const navigation = useNavigation();
   return (
     <div style={{ textAlign: 'center' }}>
       <Header />
-      <Outlet />
+      {navigation.state === 'loading' ? (
+        <p className="text-4xl">Data Loding.........</p>
+      ) : (
+        <Outlet />
+      )}
+
       <h3 style={{ textAlign: 'center' }}>This is Footer section</h3>
     </div>
   );
