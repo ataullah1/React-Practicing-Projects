fd7d14672d24ecb8d739ab11303394cb225074e9
Md Ataullah <ataullahm100@gmail.com>
2024-03-23 16:37:33 +0600

    Job catagorise added

--- careerHubProject/src/components/FeaturedJobs/FeaturedJobs.jsx
+++ careerHubProject/src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -1,7 +1,7 @@
 const FeaturedJobs = () => {
   return (
     <div className="text-center">
-      <h1 className="text-zinc-900 text-5xl font-extrabold pb-5 pt-36">
+      <h1 className="text-zinc-900 text-5xl font-extrabold pb-5 pt-24">
         Featured Jobs
       </h1>
       <p className="text-neutral-500 text-base font-medium  leading-relaxed pb-8">
--- careerHubProject/src/components/JobCategoryList/JobCategoryList.jsx
+++ careerHubProject/src/components/JobCategoryList/JobCategoryList.jsx
@@ -1,20 +1,22 @@
-const JobCategoryList = ({ jobDta }) => {
-  const { logo, category_name, availability } = jobDta;
+import PropTypes from 'prop-types';
+const JobCategoryList = ({ jobList }) => {
+  console.log(jobList);
+  const { logo, category_name, availability } = jobList;
   return (
     <div className="bg-gradient-to-r from-[#7E90FE0D] to-[#9873FF0D] rounded-lg p-5 md:p-10">
       <div className="w-[70px] h-[70px] bg-gradient-to-r from-[#7E90FE1A] to-[#9873FF1A] rounded-lg p-3">
         <img className="w-full" src={logo} alt="" />
       </div>
       <div className="text-zinc-700 text-xl font-extrabold pt-8 pb-2">
-        Account & Finance
+        {category_name}
       </div>
       <div className="text-neutral-400 text-base font-medium leading-relaxed">
-        300 Jobs Available
+        {availability}
       </div>
     </div>
   );
 };
-JobCategoryList.protoType={
-  jobDta:
-}
+JobCategoryList.propTypes = {
+  jobList: PropTypes.object,
+};
 export default JobCategoryList;
