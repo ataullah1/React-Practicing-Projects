9604109d23b0299c3e96d94db42965356c056fea
Md Ataullah <ataullahm100@gmail.com>
2024-03-23 11:52:53 +0600

    data update

--- careerHubProject/src/components/CategoriesJobs/CategoriesJob.jsx
+++ careerHubProject/src/components/CategoriesJobs/CategoriesJob.jsx
@@ -1,24 +1,32 @@
+import PropTypes from 'prop-types';
+
 const CategoriesJob = ({ job }) => {
-  const { id, logo, job_title, company_name, remote_or_onsite, location } = job;
-  console.log(job_title);
+  const {
+    logo,
+    job_title,
+    company_name,
+    remote_or_onsite,
+    job_type,
+    location,
+    salary,
+  } = job;
+  //   console.log(job_title);
   return (
     <div className="border border-gray-200 rounded-lg p-5 lg:p-10 space-y-3">
       <img className="h-12 mb-5" src={logo} alt="" />
-      <h2 className="text-zinc-700 text-2xl font-extrabold">
-        Technical Database Engineer
-      </h2>
-      <p className="text-neutral-500 text-xl font-semibold">Google LLC</p>
+      <h2 className="text-zinc-700 text-2xl font-extrabold">{job_title}</h2>
+      <p className="text-neutral-500 text-xl font-semibold">{company_name}</p>
       <div className="flex gap-5">
         <button className="px-6 py-2 rounded border border-indigo-400 text-indigo-400 text-base font-extrabold">
-          Remote
+          {remote_or_onsite}
         </button>
         <button className="px-6 py-2 rounded border border-indigo-400 text-indigo-400 text-base font-extrabold">
-          Full Time
+          {job_type}
         </button>
       </div>
       <div className="flex gap-5">
-        <p>Dhaka, Bangladesh</p>
-        <p>Salary : 100K - 150K</p>
+        <p>{location}</p>
+        <p>Salary : {salary}</p>
       </div>
       <button className="px-6 py-2 bg-gradient-to-r from-indigo-400 to-violet-500 rounded text-white text-xl font-extrabold ">
         View Details
@@ -27,4 +35,7 @@ const CategoriesJob = ({ job }) => {
   );
 };
 
+CategoriesJob.propTypes = {
+  job: PropTypes.object,
+};
 export default CategoriesJob;
