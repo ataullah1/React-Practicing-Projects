e1ca2bc38bcff7e654550d0295d6359ab766b05b
Md Ataullah <ataullahm100@gmail.com>
2024-03-23 16:52:10 +0600

    Design done

--- careerHubProject/src/components/JobList/JobList.jsx
+++ careerHubProject/src/components/JobList/JobList.jsx
@@ -1,7 +1,7 @@
 const JobList = () => {
   return (
     <div className="text-center">
-      <h1 className="text-zinc-900 text-5xl font-extrabold pb-5 pt-24">
+      <h1 className="text-zinc-900 text-3xl sm:text-5xl font-extrabold pb-5 pt-24">
         Job Category List
       </h1>
       <p className="text-neutral-500 text-base font-medium  leading-relaxed pb-8">
